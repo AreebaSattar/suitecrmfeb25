4b344d4ec4a810c6396aca12d679529ce9b805b6
Areeba Sattar <areebasattar23@gmail.com>
2025-02-25 00:29:15 +0500

    Modules loaded from Package installed

---
+++ custom/Extension/application/Ext/Include/new.php
@@ -0,0 +1,10 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['new_Brands'] = 'new_Brands';
+$beanFiles['new_Brands'] = 'modules/new_Brands/new_Brands.php';
+$moduleList[] = 'new_Brands';
+$beanList['new_Opportunities_Details'] = 'new_Opportunities_Details';
+$beanFiles['new_Opportunities_Details'] = 'modules/new_Opportunities_Details/new_Opportunities_Details.php';
+$moduleList[] = 'new_Opportunities_Details';
+
+?>
